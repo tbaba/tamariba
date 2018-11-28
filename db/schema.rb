0c2c67a200c5cc20ee458f619482b65c3c36f8ea
Tatsuro Baba <harakirisoul@gmail.com>
2018-11-28 23:56:36 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_28_145550) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "screen_name", null: false
+    t.string "account_name", null: false
+    t.string "email", null: false
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
